0c4660ba8417a1ed237fb1cd971e5c9b4662dad6
Tini <156963175+Tini369@users.noreply.github.com>
2024-04-13 22:01:09 +0200

    Add files via upload

---
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    //This enables and disables the button if there is or isnt an input
+    var userEmail = document.getElementById('email').value;
+    if ( userEmail.length === 0 ) {
+        document.getElementById("sub-btn").disabled = true;
+    } else if (userEmail.length > 0) {
+        document.getElementById("sub-btn").disabled = false;
+    }
+    //This toggles bettwen the success message and the main page
+    document.getElementById("success-msg").classList.toggle("show");
+    document.getElementById("main").classList.toggle("hide");
+    //This puts the user input(their email) into a span
+    document.getElementById("user-email").innerHTML = userEmail;
+}
