12095be69e50905dc54906dca820795843e159ab
Tini <156963175+Tini369@users.noreply.github.com>
2024-04-19 21:41:55 +0200

    index.js

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -1,15 +1,21 @@
-function myFunction(){
-    //This enables and disables the button if there is or isnt an input
-    var userEmail = document.getElementById('email').value;
-    if ( userEmail.length === 0 ) {
-        document.getElementById("sub-btn").disabled = true;
-        alert("Refresh and enter your email");
-    } else if (userEmail.length > 0) {
-        document.getElementById("sub-btn").disabled = false;
-    }
-    //This toggles bettwen the success message and the main page
-    document.getElementById("success-msg").classList.toggle("show");
-    document.getElementById("main").classList.toggle("hide");
-    //This puts the user input(their email) into a span
-    document.getElementById("user-email").innerHTML = userEmail;
+function myFunction() {
+  //This enables and disables the button if there is or isnt an input
+  var userEmail = document.getElementById("email").value;
+  if (userEmail.length === 0)
+    alert("Email field is mandatory!");
+    return;
+  }
+  //This toggles bettwen the success message and the main page
+  document.getElementById("main").classList.toggle("hide");
+  document.getElementById("success-msg").classList.toggle("show");
+  //This puts the user input into a span
+  document.getElementById("user-email").innerHTML = userEmail;
+}
+function test() {
+  var userEmail = document.getElementById("email").value;
+  if (userEmail.length === 0) {
+    document.getElementById("sub-btn").disabled = true;
+  } else {
+    document.getElementById("sub-btn").disabled = false;
+  }
 }
