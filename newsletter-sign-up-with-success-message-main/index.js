45e4f8d96114cb566cdc052858ec1d52a23434d2
Tini <156963175+Tini369@users.noreply.github.com>
2024-04-20 01:33:09 +0200

    Update index.js

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -1,17 +1,18 @@
 function myFunction() {
-  //This enables and disables the button if there is or isnt an input
   var userEmail = document.getElementById("email").value;
-  if (userEmail.length === 0)
+  if (userEmail.length === 0) {
     alert("Email field is mandatory!");
     return;
   }
+
   //This toggles bettwen the success message and the main page
   document.getElementById("main").classList.toggle("hide");
   document.getElementById("success-msg").classList.toggle("show");
   //This puts the user input into a span
   document.getElementById("user-email").innerHTML = userEmail;
 }
-function test() {
+
+function subBtn() {
   var userEmail = document.getElementById("email").value;
   if (userEmail.length === 0) {
     document.getElementById("sub-btn").disabled = true;
